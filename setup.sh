b4cc03758131f2f846fa4d0d18f7f895cde5de3a
samlyu95 <sam@aandkrobotics.com>
2019-09-18 14:28:36 -0700

    [major] add script to install vim configuration

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Install vim and vim-plug
+apt install vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Install .vimrc file
+cp $CURR_DIR/vim_config $HOME/.vimrc
+
+# Run vim-plug to install all necessary plugins
+vim +'PlugInstall --sync' +qall
+
