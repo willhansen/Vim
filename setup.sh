17fc3a8dbd74b3dc175a5c30c27d5c666b4978f2
Will Hansen <will.r.s.hansen@gmail.com>
2020-09-05 00:11:50 -0700

    Use the system clipboard for copy and paste
    Also install a version of vim that supports that

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Install vim and vim-plug
-apt install vim
+apt install vim-gtk3
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Install .vimrc file
@@ -11,4 +11,3 @@ cp $CURR_DIR/vim_config $HOME/.vimrc
 
 # Run vim-plug to install all necessary plugins
 vim +'PlugInstall --sync' +qall
-
